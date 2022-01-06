70a9fa2fc2f22931bea344fe29d2e86539bd4e25
unknown <tempest1389@gmail.com>
2022-01-06 17:29:57 +0900

    2022/01/06 자바 Test 연습문제

---
+++ Test/src/test1/Ex04.java
@@ -0,0 +1,27 @@
+package test1;
+/*
+ * 날짜 : 2022/01/06
+ * 이름 : 양성민
+ * 내용 : 자바 연산자 연습문제
+ */
+public class Ex04 {
+	public static void main(String[] args) {
+		
+		int num = 1;
+		int result;
+		
+		result = num++;
+		System.out.println("result : "+result);
+		
+		result = ++num;
+		System.out.println("result : "+result);
+		
+		result = num--;
+		System.out.println("result : "+result);
+		
+		result = --num;
+		System.out.println("result : "+result);
+		
+	}
+
+}
