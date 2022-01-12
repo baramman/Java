ef69f4b7ff305fa9ba74232a6b49e0934a8af247
unknown <tempest1389@gmail.com>
2022-01-12 13:17:10 +0900

    2022/01/12 연습문제

---
+++ Test/src/test4/Ex05.java
@@ -0,0 +1,16 @@
+package test4;
+/*
+ * 날짜 : 2022/01/12
+ * 이름 : 양성민
+ * 내용 : String, Wrapper 클래스 연습문제
+ */
+public class Ex05 {
+	public static void main(String[] args) {
+		
+		String strCsv = "60,72,82,86,92";
+		String[] scores = strCsv.split(",");
+		
+		int total = 0;
+	}
+
+}
